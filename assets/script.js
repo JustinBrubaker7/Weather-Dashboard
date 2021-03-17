74df3375eb909ba709564ab07a0ed3c63f9b6290
Justin Brubaker <justinbrubaker7@gmail.com>
2021-03-16 19:40:17 -0700

    added recent searches function

--- assets/script.js
+++ assets/script.js
@@ -8,35 +8,19 @@ var weatherCardElday3 = document.getElementById("weather-card-5day-3");
 var weatherCardElday4 = document.getElementById("weather-card-5day-4");
 var weatherCardElday5 = document.getElementById("weather-card-5day-5");
 var weatherCardHeading = document.getElementById("weather-card-heading");
+var forcastHeading = document.getElementById("5-day-forcast-title");
 var bodyEl = document.getElementById("main");
 var pastSearchesEl = document.getElementById("past-searches");
 
-var citySearchList = [];
+
 
 function getApi(citySearchTerm) {
     
     var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + citySearchTerm + '&units=imperial&appid=9a32d5e00225681fffab293fb6a516e1';
-//     function setRecentSearches(){ 
- 
-
-//     citySearchList.unshift(citySearchTerm);
-//     window.localStorage.setItem("city", JSON.stringify(citySearchList));
-//     var cityResults = JSON.parse(window.localStorage.getItem("city"));
-//     var citySearchResults = document.getElementById("past-searches");
-//     //citySearchResults.value = cityResults;
-
-//     for(var i = 0; i > citySearchList.length; i++){
-//         var pastSeachLi = document.createElement("li");
-//         pastSeachLi.innerHTML = citySearchList[i];
-//         pastSearchesEl.appendChild(pastSeachLi)
-//     }
-//     console.log(citySearchList)
-//    }
-
 
     fetch(requestUrl)
       .then(function (response) {
-        console.log(response.status)
+        //console.log(response.status)
           if(response.status == 200){
             return response.json();
           } else {
@@ -65,7 +49,8 @@ function getApi(citySearchTerm) {
     document.getElementById('wind-speed').innerHTML = "Wind Speed: " + Math.round(data.wind.speed) + "mph";
     document.getElementById('clouds').innerHTML = "Cloud Coverage: " + data.clouds.all + "%";
     weatherCardEl.setAttribute("id", "weather-card2");
-    console.log(mainWeather);
+    forcastHeading.textContent = "5-Day Forcast";
+    //console.log(mainWeather);
 
    if(mainWeather == "Clear"){
     bodyEl.setAttribute("class", "clear");
@@ -107,15 +92,6 @@ function getApi(citySearchTerm) {
   }
 
 
-//   window.onload = function(){
-
-//     document.getElementById("past-searches").innerHTML = JSON.parse(window.localStorage.getItem("city"));
-//     //document.getElementById("past-searches").text = JSON.parse(window.localStorage.getItem("city"));
-//   }
-
-
-
-
 function getApiForcast(citySearchTerm) {
     var requestUrl5day = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearchTerm + "&units=imperial&appid=9a32d5e00225681fffab293fb6a516e1"
 
@@ -139,34 +115,61 @@ function getApiForcast(citySearchTerm) {
 
 
   function displayDataForcast(data){
-    document.getElementById('tempurature1').innerHTML = Math.round(data.list[0].main.feels_like) + '&deg;';
+    document.getElementById('tempurature1').innerHTML = Math.round(data.list[3].main.feels_like) + '&deg;';
     document.getElementById('city1').innerHTML = data.city.name;
     weatherCardElday1.setAttribute("id", "weather-cardday2");
 
     //day 2
-    document.getElementById('tempurature2').innerHTML = Math.round(data.list[1].main.feels_like) + '&deg;';
+    document.getElementById('tempurature2').innerHTML = Math.round(data.list[11].main.feels_like) + '&deg;';
 	  document.getElementById('city2').innerHTML = data.city.name;
     weatherCardElday2.setAttribute("id", "weather-cardday3");
 
     //day 3
-    document.getElementById('tempurature3').innerHTML = Math.round(data.list[2].main.feels_like) + '&deg;';
+    document.getElementById('tempurature3').innerHTML = Math.round(data.list[19].main.feels_like) + '&deg;';
     document.getElementById('city3').innerHTML = data.city.name;
     weatherCardElday3.setAttribute("id", "weather-cardday4");
 
    //day 4
-    document.getElementById('tempurature4').innerHTML = Math.round(data.list[3].main.feels_like) + '&deg;';
+    document.getElementById('tempurature4').innerHTML = Math.round(data.list[27].main.feels_like) + '&deg;';
     document.getElementById('city4').innerHTML = data.city.name;
     weatherCardElday4.setAttribute("id", "weather-cardday5");
 
     //day 5
-    document.getElementById('tempurature5').innerHTML = Math.round(data.list[4].main.feels_like) + '&deg;';
+    document.getElementById('tempurature5').innerHTML = Math.round(data.list[34].main.feels_like) + '&deg;';
     document.getElementById('city5').innerHTML = data.city.name;
     weatherCardElday5.setAttribute("id", "weather-cardday6");
 }
 
 
+var citySearchList = [];
+var cityListItem1 = document.getElementById("city-list-1");
 
+//Setting recent searches to local storage
 
+window.onload = function(){
+ var citySearchListArray = JSON.parse(window.localStorage.getItem("city"));
+  getFromLocalStorage(citySearchListArray)
+  citySearchList = citySearchListArray
+}
+
+function getFromLocalStorage(data) {
+ 
+  if(data != ""){
+    for(var i = 0; i < 3; i++){
+      var listItemEl = document.createElement("li");
+      listItemEl.textContent = data[i];
+      pastSearchesEl.appendChild(listItemEl);
+      listItemEl.setAttribute("class", "text-white bg-dark m-3 p-3 text-center col-12");
+      listItemEl.onclick = function(){
+        var textContentofLi = listItemEl.innerHTML;
+        getApi(textContentofLi);
+        getApiForcast(textContentofLi)
+      };
+    }
+
+  }
+
+}
 
 
 // NEEDS TO BE LAST
@@ -175,11 +178,10 @@ function getApiForcast(citySearchTerm) {
         e.preventDefault();
         var seachBoxValue = searchBoxEl.value.trim();
         getApi(seachBoxValue);
-        getApiForcast(seachBoxValue)
+        getApiForcast(seachBoxValue);
+        citySearchList.push(seachBoxValue);
+        console.log(citySearchList);
+        window.localStorage.setItem('city', JSON.stringify(citySearchList));
 });
-
-
-
-
 
 
